833239673d7f68e3b430ba389748edfc38734d0e
hatimcontractor <148803018+hatimcontractor@users.noreply.github.com>
2023-11-07 14:58:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,10 @@ from model import PredictReview
 
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #f0f8ff; /* Light blue background color */
-    }
-    </style>
-    """,
+st.write(
+    '<style>'
+    'body { background-color: #f0f8ff; }'  # Light blue background color
+    '</style>',
     unsafe_allow_html=True
 )
 
